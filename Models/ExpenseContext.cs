d2ed7b26e25452e91dd534e51dd625a3c59b48ad
ALEXANDRA-ELENA URSU <alexandra.elena.ursu@stud.ubbcluj.ro>
2021-11-04 18:48:17 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ursu_Alexandra_Lab5EB.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
